ae1bf680a7611de387fdeae21e1a1839bcc54d97
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-07-14 19:07:42 +0300

    Project start

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./assets/styles/app.css";
+import Header from "./components/Header/Header";
+import Categories from "./components/Categories/Categories";
+import Sort from "./components/Sort/Sort";
+import PizzaBlock from "./components/PizzaBlock/PizzaBlock";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Header />
+        <div className="content">
+          <div className="container">
+            <div className="content__top">
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+              <PizzaBlock />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Categories = () => {
+  return (
+    <nav className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Categories;
